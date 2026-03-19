c9481a57a258789b8c1c4cbd5d5a9beeb1129f7b
Danazuminemesis <muhammadrabiumuhammad0105@gmail.com>
2026-03-19 16:45:26 +0100

    added the scoring system

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+from flask import request
+from datetime import datetime
+
 @app.route("/")
 def home():
     return render_template("index.html")
 if __name__ == "__main__":
     app.run(debug=True)
 
-@app.route("/test")
+@app.route("/test", methods=["GET", "POST"])
 def test():
+    if request.method == "POST":
+        score = 0
+
+        for i, question in enumerate(questions):
+            answer = request.form.get(f"q{i}")
+            if answer == question.answer:
+                score += 1
+
+        time_submitted = datetime.now().strftime("%H:%M")
+
+        return render_template("result.html", score=score, total=len(questions), time=time_submitted)
+
     return render_template("test.html", questions=questions)
 
 from models import Question
