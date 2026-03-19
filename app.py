0cc3b1ce5f1061c8ba6118942e7e19682381ee5b
Danazuminemesis <muhammadrabiumuhammad0105@gmail.com>
2026-03-19 16:23:36 +0100

    added initial lists of questions

--- app.py
+++ app.py
@@ -5,3 +5,10 @@ def home():
     return render_template("index.html")
 if __name__ == "__main__":
     app.run(debug=True)
+
+from models import Question
+questions = [
+    Question("What is Python?", "Programming Language"),
+    Question("What is 2 + 2?", "4"),
+    Question("Which is a data structure?", "Queue")
+]
